5d29b675cfb380ab613cad096691e858b85a67db
Alexandr <akatsuki_sanya@vk.com>
2025-04-15 21:04:20 +0500

    Переписал Settings на библиотеке pydantic-settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,10 @@
-from pydantic_settings import BaseSettings
-from pydantic import Extra
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_title: str = "Бронирование переговорок"
-
-    class Config:
-        env_file = ".env"
-        extra = Extra.allow
+    app_title: str = "Тестовое задание"
+    model_config = {"extra": "allow"}
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
 
 settings = Settings()
