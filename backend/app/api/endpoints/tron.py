cda919529177371c81b0413b59b10131ec2c4f51
Alexandr <akatsuki_sanya@vk.com>
2025-04-17 14:15:48 +0500

    Сделал гет ендпоинт на получение записей в бд

--- backend/app/api/endpoints/tron.py
+++ backend/app/api/endpoints/tron.py
@@ -1,10 +1,14 @@
+from fastapi import APIRouter
+from fastapi.params import Depends
 from tronpy import Tron
 from tronpy.providers.http import HTTPProvider
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi import APIRouter
-from app.schemas.tron import TronAddressRequest
-from app.schemas.dto import FailedResponse, SuccessResponse
+from app.core.db import get_async_session
 from app.core.config import settings
+from app.crud.tron import tron_crud
+from app.schemas.dto import FailedResponse, SuccessResponse
+from app.schemas.tron import TronAddressRequest
 
 router = APIRouter()
 
@@ -15,7 +19,7 @@ router = APIRouter()
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
 )
-async def create_new_donation(
+async def get_wallet_info(
     address: TronAddressRequest,
 ):
     try:
@@ -43,3 +47,16 @@ async def create_new_donation(
 
     except Exception as e:
         return FailedResponse(status=False, data={None}, message=str(e))
+
+
+@router.get(
+    "/",
+    response_model=SuccessResponse,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+)
+async def get_data(session: AsyncSession = Depends(get_async_session)):
+    return SuccessResponse(
+        status=True,
+        data={"Последние записи": await tron_crud.get_multi(session=session)},
+    )
